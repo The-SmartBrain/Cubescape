27334186fb9d569aa78bc143967f8c8868fd5e56
TheSmartBrain <fmail@czuweb.de>
2026-04-12 15:44:40 +0200

    Simple GUI support

--- src/menu_scene.zig
+++ src/menu_scene.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 const rl = @import("raylib");
+const raygui = @import("raygui");
 const SceneContext = @import("scene/context.zig").SceneContext;
 const SceneId = @import("scene/id.zig").SceneId;
 
 pub const MenuScene = struct {
     allocator: std.mem.Allocator,
+    currentOption: i32 = 0,
 
     pub fn onStartup(self: *MenuScene, context: *SceneContext) anyerror!void {
         std.log.info("Starting Menu scene", .{});
@@ -12,20 +14,44 @@ pub const MenuScene = struct {
         self.allocator = context.allocator;
 
         // Init Scene here --> Läuft EINMAL beim Start
+
+        self.currentOption = 0;
     }
 
     pub fn onUpdate(self: *MenuScene, context: *SceneContext, delta_time: f32) anyerror!void {
         // main Loop
-        _ = self;
         _ = delta_time;
 
         if (rl.isKeyPressed(.enter)) {
             try context.switchTo(SceneId.game);
             return;
         }
-        rl.clearBackground(.blue);
-        rl.drawText("Menu Scene", 40, 40, 64, .white);
-        rl.drawText("Press Enter to start the game.", 40, 120, 36, .white);
+        rl.clearBackground(.ray_white);
+        _ = raygui.label(rl.Rectangle{ .x = 100, .y = 100, .width = 200, .height = 40 }, "Main Menu");
+
+        // Button für den Spielstart
+        if (raygui.button(rl.Rectangle{ .x = 100, .y = 150, .width = 200, .height = 40 }, "Start Game")) {
+            self.currentOption = 1; // Spiel starten
+        }
+
+        // Button für Einstellungen
+        if (raygui.button(rl.Rectangle{ .x = 100, .y = 200, .width = 200, .height = 40 }, "Settings")) {
+            self.currentOption = 2; // Einstellungen öffnen
+        }
+
+        // Button für die Levelauswahl
+        if (raygui.button(rl.Rectangle{ .x = 100, .y = 250, .width = 200, .height = 40 }, "Beenden")) {
+            self.currentOption = 3; // Levelauswahl öffnen
+        }
+
+        // Weitere logik für die Optionen
+        if (self.currentOption == 1) {
+            _ = raygui.label(rl.Rectangle{ .x = 100, .y = 300, .width = 200, .height = 40 }, "Game Started...");
+        } else if (self.currentOption == 2) {
+            _ = raygui.label(rl.Rectangle{ .x = 100, .y = 300, .width = 200, .height = 40 }, "Settings Screen...");
+        } else if (self.currentOption == 3) {
+            _ = raygui.label(rl.Rectangle{ .x = 100, .y = 300, .width = 200, .height = 40 }, "Shutting down...");
+        }
     }
 
     pub fn onCleanup(self: *MenuScene, context: *SceneContext) anyerror!void {
